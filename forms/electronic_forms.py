75294a37d81e14038201a13ea7ea80a02fbc9b5b
mehdi hossieni <39370153+meidix@users.noreply.github.com>
2020-08-03 17:54:29 +0430

    the electronicApplicantCreationForm added.working on the templates and decorating them

---
+++ forms/electronic_forms.py
@@ -0,0 +1,177 @@
+from flask_wtf import FlaskForm
+from wtforms import (
+    StringField, validators, DateField, TextAreaField, SelectField, SubmitField, RadioField, FileField
+)
+
+from . import models
+
+DEGREE_CHOICES = [
+    (models.UniversityDegreeEnum.diploma, 'دیپلم'),
+    (models.UniversityDegreeEnum.associate, 'فوق دیپلم'),
+    (models.UniversityDegreeEnum.bachelor,'کارشناسی'),
+    (models.UniversityDegreeEnum.senior, 'کارشناسی ارشد'),
+    (models.UniversityDegreeEnum.phd, 'دکتری')]
+
+
+class ApplicantsCreationForm(FlaskForm):
+    first_name = StringField(
+        label="نام",
+        validators=[
+            validators.DataRequired('وارد کردن نام الزامیست'),
+            validators.Length(max=30)
+        ]
+    )
+    
+    last_name = StringField(
+        label='نام خانوادگی',
+        validators=[
+            validators.DataRequired('وارد کردن نام خانوادگی الزامیست'),
+            validators.Length(max=45)
+        ]
+    )
+
+    national_id = StringField(
+        label='کد ملی',
+        validators=[
+            validators.DataRequired('وارد کردن کد ملی الزامیست'),
+            validators.Length(min=10, max=10, message='کد ملی به درستی وارد نشده')
+        ]
+    )
+
+    date_of_birth = DateField(
+        label='تاریخ تولد'
+    )
+
+    email = StringField(
+        label='ایمیل',
+        validators=[
+            validators.Email('آدرس ایمیل وارد شده صحیح نمیباشد'),
+            validators.InputRequired('وارد کردن ایمیل الزامیست')
+        ]
+    )
+
+    mobile_phone = StringField(
+        label='شماره موبایل',
+        validators=[
+            validators.Length(min=11, max=11, message='شماره موبایل وارد شده صحیح نمی باشد'),
+            validators.InputRequired('وارد کردن شماره موبایل الزامیست')
+        ]
+    )
+
+    Address = TextAreaField(
+        label='آدرس',
+        validators=[
+            validators.Length(max=500)
+        ]
+    )
+
+    university = StringField(
+        label='دانشگاه',
+        validators=[
+            validators.InputRequired('وارد کردن دانشگاه محل تحصیل الزامیست'),
+        ]
+    )
+
+    university_subject = StringField(
+        label='رشته تحصیلی',
+        validators=[
+            validators.InputRequired('وارد کردن رشته تحصیلی الزامیست'),
+        ]
+    )
+
+    university_degree = SelectField(
+        label='مدرک تحصیلی',
+        validators=[
+            validators.InputRequired('وارد کردن مدرک تحصیلی الزامیست')
+        ],
+        choices=DEGREE_CHOICES
+    )
+
+    work_reputations = TextAreaField(
+        label='سوابق کاری',
+        validators=[validators.Optional()]
+    )
+
+    submit = SubmitField()
+
+
+class ElectricalApplicantCreationForm(ApplicantsCreationForm):
+    altium_designer = RadioField(
+        label='آلتیوم دیزاینر(Altium Designer)',
+        validators=[
+            validators.Optional()
+        ]
+    )
+
+    arduino = RadioField(
+        label='آردوینو(Arduino)',
+        validators=[
+            validators.Optional()
+        ]
+    )
+
+    code_vision = RadioField(
+        label='کد ویژن(code vision)',
+        validators=[
+            validators.Optional()
+        ]
+    )
+
+    proteus = RadioField(
+        label='پروتئوس(proteus)',
+        validators=[
+            validators.Optional()
+        ]
+    )
+
+    atmel_studio = RadioField(
+        label='اتمل استودیو(Atmel Studio)',
+        validators=[
+            validators.Optional()
+        ]
+    )
+
+    proteus = RadioField(
+        label='پروتئوس(proteus)',
+        validators=[
+            validators.Optional()
+        ]
+    )
+
+    microcontroller = RadioField(
+        label='میکروکنترلر ها',
+        validators=[
+            validators.Optional()
+        ]
+    )
+
+    power = RadioField(
+        label='مدارات تغذیه',
+        validators=[
+            validators.Optional()
+        ]
+    )
+
+    others = TextAreaField(
+        label='مهارت های دیگر',
+        validators=[
+            validators.Optional()
+        ]
+    )
+
+    resume = FileField(
+        label='رزومه',
+        validators=[
+            validators.Optional()
+        ]
+    )
+
+    expected_salary = StringField(
+        label='حقوق پیشنهادی',
+        validators=[
+            validators.Length(max=7, message=':))))')
+        ]
+    )
+
+
+    
--- forms/views/electronics.py
+++ forms/views/electronics.py
@@ -2,7 +2,8 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
 
-from forms.models import db
+from forms.models import db, Applicant, ElectronicApplicant
+from forms.electronic_forms import ApplicantsCreationForm, ElectricalApplicantCreationForm
 
 
 bp = Blueprint('electronics', __name__)
@@ -14,9 +15,36 @@ def index():
 
 @bp.route('/form', methods=['GET', 'POST'])
 def create_request():
-    form = None
+    form = ElectricalApplicantCreationForm()
 
-    if request.method == 'POST':
-        pass
+    if form.validate_on_submit():
+        applicant = ElectronicApplicant(
+            first_name=form.first_name,
+            last_name=form.last_name,
+            national_id=form.national_id,
+            date_of_birth=form.date_of_birth,
+            email=form.email,
+            mobile_phone=form.mobile_phone,
+            Address=form.Address,
+            university=form.university,
+            university_subject=form.university_subject,
+            university_degree=form.university_degree,
+            work_reputation=form.work_reputations,
+            altium_designer=form.altium_designer,
+            arduino=form.arduino,
+            proteus=form.proteus,
+            atmel_studio=form.atmel_studio,
+            microcontroller=form.microcontroller,
+            power=form.power,
+            others=form.others,
+            resume=form.resume,
+            expected_salary=form.expected_salary
+        )
+        db.session.add(applicant)
+        db.session.commit()
 
-    return render_template('electronics/emplyment-form.html', form=form)
+        return redirect(url_for('index'))
+    
+    else:
+        flash('لطفا ایرادات زیر را قبل از ارسال دوباره فرم برطرف کنید', 'danger')
+        return render_template('electronics/employment-form.html', form=form)
